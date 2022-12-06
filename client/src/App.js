e2d836b72951761c481fafe76158dad3d20a6c39
Anjaney C Mahajan <acmahaja@gmail.com>
2022-12-06 12:38:03 +0200

    Added example server backend

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,23 @@
 import './App.css';
+import { Routes, Route, Outlet, Link } from "react-router-dom";
+import Dashboard from './Routes/Dashboard'
+import Home from './Routes/Home'
 
 function App() {
+
+
+  
+  
   return (
     <div className="App">
       <header>
         <h1>Mern Template</h1>
       </header>
+      <Routes >
+        <Route index element={<Home />} />
+        <Route path="dashboard" element={<Dashboard />} />
+      </Routes >
+
     </div>
   );
 }
---
+++ client/src/Routes/Dashboard.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+
+function Dashboard() {
+    return (
+        <h2>Dashboard</h2>
+    )
+}
+
+export default Dashboard;
